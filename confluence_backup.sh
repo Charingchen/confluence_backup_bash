d6aa9375f0c6e41c553d1cf6db369ddcb30f5614
Charing Chen <charinghaxen@gmail.com>
2020-06-24 16:21:41 -0700

    initial Commit

--- confluence_backup.sh
+++ confluence_backup.sh
@@ -1,25 +1,24 @@
-# Confluence Backup Bash Script
-This scripts is used to back up Confluence self host server production on a Linux machine.
-The goal is to zip the entire home directory of confluence and export the MySQL database that connected to the confluence server.
+#!/bin/bash
 
-## Zip Confluence home directory
-Uses tar command:
-```bash
+#################################################
+### Backing up Confluence home directory
+
 BACKUPTIME=`date +%b-%d-%y` #get the current date
 HOME_BACKUP=/home/confluence_backup/backup-$BACKUPTIME.tar.gz #create a backup file using the current date in it's name
 SOURCEFOLDER=/var/atlassian/application-data #the folder that contains the files that we want to backup
 
-tar -cpzf $HOME_BACKUP $SOURCEFOLDER #create the backup
-```
 
-## Backup MySQL using scripts
-MySQL code is referece from https://conetix.com.au/support/scripted-mysql-database-backups/
-Here are the functions I reference from the previous website:
-```bash
+#################################################
+# Backing up Confluence Database
+# use mysqldump to Dump DB and compress it on the fly to a mounted partition
+
 BACKUP_DIR="/home/confluence_backup/db-backups"
 mkdir -p $BACKUP_DIR
 chmod 777 $BACKUP_DIR
+#
+#
 SERIAL="`date +%b-%d-%y`"
+
 #=====================================
 # Log Functions
 #
@@ -35,7 +34,10 @@ echo " " >> $LF
 echo "Time: `date`" >> $LF
 echo "====== Log End   =========" >> $LF
 }
+
 #=====================================
+#
+#
 function DoBackup
 {
 echo "Calling DoBackup()" >> $LF
@@ -53,10 +55,7 @@ echo "Zipping up file!" >> $LF
 gzip ${DBFILE}
 echo "Done!" >> $LF
 }
-```
-Note mysqldump need input user and its password to perform the dump. Here I harded code the password in the script.
-Setting up the log file for mysqldump:
-```bash
+
 FILE_DATE=`date '+%Y-%m-%d'`
 LF_DIR=/home/confluence_backup/db-backups/logs/db-backup
 LF=$LF_DIR/db-backup-$FILE_DATE.log
@@ -66,14 +65,29 @@ touch $LF
 chmod 664 $LF
 
 DBLIST=/tmp/dblist-$FILE_DATE.list
-```
-Instead of dumping all the databases, I only need the one connect to the confluence.
 
-```bash
+LogStart
+#=====================================
+#
+#                     MAIN Code Start
+echo "Start zipping conflunece home directory" >> $LF
+tar -cpzf $HOME_BACKUP $SOURCEFOLDER #create the backup
+echo "Done Zipping, saved at $SOURCEFOLDER" >>$LF
+
+
 line="[confluence database name]"
 echo "Backuping up Database: $line" >>$LF
 H="localhost"
 DB=$line
 DoBackup
 echo "All backups Completed" >> $LF
-```
+
+
+############################################
+### File transfer code
+# connect via scp
+scp $HOME_BACKUP user@remotebackhost:/home/backup
+echo "confluence zip sending Done" >>$LF
+scp $DBFILE.gz user@remotebackhost:/home/backup
+echo "Database backup sending Done">>$LF
+LogEnd
